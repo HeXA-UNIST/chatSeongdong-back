467788300166a2e86559ff8f5f8493bee71ec099
taeyoonh <taeyoonh@unist.ac.kr>
2023-10-27 11:23:53 +0900

    init backend

---
+++ demo/src/main/java/com/pro/chatgpt/model/response/Choice.java
@@ -0,0 +1,38 @@
+package com.pro.chatgpt.model.response;
+
+import com.pro.chatgpt.model.common.Message;
+
+public class Choice {
+
+    public int index;
+    public Message message;
+    public String finish_reason;
+
+    public Choice() {
+    }
+
+    public int getIndex() {
+        return index;
+    }
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public Message getMessage() {
+        return message;
+    }
+
+    public void setMessage(Message message) {
+        this.message = message;
+    }
+
+    public String getFinish_reason() {
+        return finish_reason;
+    }
+
+    public void setFinish_reason(String finish_reason) {
+        this.finish_reason = finish_reason;
+    }
+
+}
